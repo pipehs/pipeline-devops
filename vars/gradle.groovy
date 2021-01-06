5874283a2d77d8db8022bf05c210c10df4ac31af
Felipe Herrera <fherrera@itappsolutions.com>
2021-01-06 16:20:10 -0300

    Agregado downloadNexus

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -57,7 +57,7 @@ def nexusCI() {
     packages: [[$class: 'MavenPackage',
         mavenAssetList: [[classifier: '',
             extension: 'jar',
-            filePath: '/root/.jenkins/workspace/ltibranch-pipeline_feature-nexus/build/DevOpsUsach2020-0.0.1.jar']],
+            filePath: "/root/.jenkins/workspace/ltibranch-pipeline_feature-nexus/build/DevOpsUsach2020-0.0.1-${GIT_BRANCH}.jar"]],
             mavenCoordinate: [
                 artifactId: 'DevOpsUsach2020',
                 groupId: 'com.devopsusach2020',
@@ -82,7 +82,7 @@ def nexusCD() {
     packages: [[$class: 'MavenPackage',
         mavenAssetList: [[classifier: '',
             extension: 'jar',
-            filePath: '/root/.jenkins/workspace/ltibranch-pipeline_feature-nexus/build/DevOpsUsach2020-0.0.1.jar']],
+            filePath: "/root/.jenkins/workspace/ltibranch-pipeline_feature-nexus/build/DevOpsUsach2020-0.0.1-${GIT_BRANCH}.jar"]],
             mavenCoordinate: [
                 artifactId: 'DevOpsUsach2020',
                 groupId: 'com.devopsusach2020',
