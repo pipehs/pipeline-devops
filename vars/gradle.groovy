99ee8a6289441e27b8723489347e19e8db79b2cf
Felipe Herrera <fherrera@itappsolutions.com>
2021-01-06 18:24:39 -0300

    Separando stages CI y CD v4

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -57,7 +57,7 @@ def nexusCI() {
     packages: [[$class: 'MavenPackage',
         mavenAssetList: [[classifier: '',
             extension: 'jar',
-            filePath: "/root/.jenkins/workspace/ltibranch-pipeline_feature-nexus/build/DevOpsUsach2020-0.0.1-develop.jar"]],
+            filePath: "/root/.jenkins/workspace/${JOB_NAME}/build/DevOpsUsach2020-0.0.1-develop.jar"]],
             mavenCoordinate: [
                 artifactId: 'DevOpsUsach2020',
                 groupId: 'com.devopsusach2020',
@@ -85,7 +85,7 @@ def nexusCD() {
     packages: [[$class: 'MavenPackage',
         mavenAssetList: [[classifier: '',
             extension: 'jar',
-            filePath: "/root/.jenkins/workspace/ltibranch-pipeline_feature-nexus/build/DevOpsUsach2020-release-v1-0-0.jar"]],
+            filePath: "/root/.jenkins/workspace/${JOB_NAME}/build/DevOpsUsach2020-release-v1-0-0.jar"]],
             mavenCoordinate: [
                 artifactId: 'DevOpsUsach2020',
                 groupId: 'com.devopsusach2020',
